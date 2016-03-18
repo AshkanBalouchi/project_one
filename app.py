cf0ef08cd59b20d2c8f2ba4a98f7c95f6e7c1041
Ashkan Balouchi <abalou1@lsu.edu>
2016-03-18 12:04:24 -0400

    try12: simplejson

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect
 import requests
-import simplejason as json
+import simplejson as json
 #from Quandl import get
 #import quandl
 #from quandl import Quandl
