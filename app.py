f6e48431cda02c474ad094df4f0e3856e454396e
Ashkan Balouchi <abalou1@lsu.edu>
2016-03-18 01:29:50 -0400

    try9 Quandl

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def index():
 	return render_template('end.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.rum(port=33507)
 
 #from flask import Flask, render_template, request, redirect
 
