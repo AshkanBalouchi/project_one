05ffc5b5e4ade7dd6acc2c65394e329a5c17540f
Ashkan Balouchi <abalou1@lsu.edu>
2016-03-18 00:53:01 -0400

    try6

--- app.py
+++ app.py
@@ -49,7 +49,8 @@ def index():
 
 
         y='WIKI/%s'%(x)
-        mydata = Quandl.get(y, authtoken="nWMHwtzyAfUAE9nJPZ82",rows=30)
+#        mydata = Quandl.get(y, authtoken="nWMHwtzyAfUAE9nJPZ82",rows=30)
+        mydata = Quandl.get(y,rows=30)
         NumLines=C+AC+O+AO
 
         colors={}
