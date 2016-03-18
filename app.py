ae9259026e237db5c07b06dff0b44a2ad2dc2ec9
Ashkan Balouchi <abalou1@lsu.edu>
2016-03-18 11:27:28 -0400

    try10.5!

--- app.py
+++ app.py
@@ -51,10 +51,14 @@ def index():
         AO=var['AdjOpen']
 
 
+       api_url = 'https://www.quandl.com/api/v1/datasets/WIKI/%s.json' % stock
+       session = requests.Session()
+       session.mount('http://', requests.adapters.HTTPAdapter(max_retries=3))
+       raw_data = session.get(api_url)
 #        api_url_csv = 'https://www.quandl.com/api/v1/datasets/WIKI/%s.csv' % stock
 #        mydata = pandas.read_csv(api_url_csv)	
-        y='WIKI/%s'%(stock)
-        mydata = Quandl.get(y, authtoken="nWMHwtzyAfUAE9nJPZ82",rows=30)
+#        y='WIKI/%s'%(stock)
+#        mydata = Quandl.get(y, authtoken="nWMHwtzyAfUAE9nJPZ82",rows=30)
 #        mydata = Quandl.get(y,rows=30)
         NumLines=C+AC+O+AO
 
